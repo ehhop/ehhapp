0b8415b295412ea4c455f3f2b1746037869caaef
finkelm <finkel.mark@gmail.com>
2013-11-19 23:22:01 -0500

    dropdown list complete?

--- templates/dropdown_list.rb
+++ templates/dropdown_list.rb
@@ -2,50 +2,51 @@ module GitWiki
 
 class DropdownList < TemplateTransformation
   def transform
-    nk_html = @nk.to_html
 
-    # convert to dropdown list
-    i = 0
-    processed = nk_html.to_s.gsub /\+d\s(.*?)(%begin)(.*?)(end%)/m do
-      i+=1
-      if i == 1
-        "<div data-role=\"collapsible-set\"><div data-role=\"collapsible\" data-collapsed=\"true\"><h3><b>#{$1}</b></h3>#{$3}</div>" 
-      else
-        "<div data-role=\"collapsible\" data-collapsed=\"true\"><h3><b>#{$1}</b></h3>#{$3}</div>"
-      end
-    end
-    processed << "</div>"
+    collapsible_set = Nokogiri::HTML.parse("<div data-role=\"collapsible-set\"></div>").at_css('div')
+    start_point = @nk.at_css('h3')
+    start_point.add_previous_sibling(collapsible_set)
+    collapsible_set = @nk.at_css('div[data-role=collapsible-set]')
+    tmp_index = 1
+    curr_element = start_point
+    curr_collapsible = nil
     
-    junk_num = 0
-    # convert -c to checklist
-    processed = processed.gsub /((?:^-c.*\n)+)/ do
-        c_block = $1
-        ret = "<fieldset data-role=\"controlgroup\">"
-        matches = c_block.scan(/-c((?:(?!-c).)*)/)
-        junk_num = 0
-        matches.each do |item|
-          junk_num+=1
-          ret << "<input type=\"checkbox\" name=\"checkbox-#{junk_num}\" id=\"checkbox-#{junk_num}\" class=\"custom\" data-mini=\"true\"/><label for=\"checkbox-#{junk_num}\">#{item[0]}</label>"
+    while true
+        break unless curr_element
+        next_element = curr_element.next_element
+        curr_element.unlink
+        if curr_element.name == 'h3'
+            collapsible = Nokogiri::HTML.parse("<div data-role=\"collapsible\" data-collapsed=\"true\"><h3><b>#{curr_element.content}</b></h3></div>").at_css('div')
+            curr_collapsible = collapsible_set.add_child(collapsible)
+        elsif curr_element.name == 'blockquote'
+            fieldset = Nokogiri::HTML.parse("<fieldset data-role=\"controlgroup\"></fieldset>")
+            curr_element.css('li').each do |li_nk|
+                checkbox = Nokogiri::HTML.parse("<input type=\"checkbox\" name=\"checkbox-#{tmp_index}\" id=\"checkbox-#{tmp_index}\" class=\"custom\" data-mini=\"true\"/>")
+                label = Nokogiri::HTML.parse("<label for=\"checkbox-#{tmp_index}\">#{li_nk.content}</label>")
+                tmp_index += 1
+                fieldset.at_css('fieldset').add_child(checkbox.at_css('input'))                
+                fieldset.at_css('fieldset').add_child(label.at_css('label'))
+            end
+            curr_collapsible.add_child(fieldset.at_css('fieldset'))
+        else
+            curr_collapsible.add_child(curr_element)
         end
-        ret << "</fieldset>"
-        ret
+        curr_element = next_element
     end
-    
-    # convert -r to radio button
-
-    processed
+    @nk.to_html
   end
 
-  example "* Dropdown Heading %begin -put stuff here- end%", <<-HTML
-      <div data-role="collapsible" data-collapsed="true"><h3><b>Dropdown Heading</b></h3></div>
+  example "### Dropdown Header\n>+ option1\n> continuation\n>+ option2\n>+ option3\n> yet another continuation", <<-HTML
+      <div data-role="collapsible" data-collapsed="true"><h3><b>Dropdown Header</b></h3><fieldset data-role="controlgroup">
+	  <input type="checkbox" name="checkbox-2" id="checkbox-2" class="custom" data-mini="true" />
+      <label for="checkbox-2">option1 continuation</label>
+      <input type="checkbox" name="checkbox-2a" id="checkbox-2a" class="custom" data-mini="true" />
+      <label for="checkbox-2a">option2</label>
+ 	  <input type="checkbox" name="checkbox-1" id="checkbox-1" class="custom" data-mini="true" />
+      <label for="checkbox-1">option3 yet another continuation</label>
+</fieldset></div>
   HTML
   
-  example "-c Checklist Item", <<-HTML
-      <fieldset data-role="controlgroup">
-	    <input type="checkbox" name="checkbox-1a" id="checkbox-1a" class="custom" data-mini="true" />
-        <label for="checkbox-1a">Checklist Item</label>
-      </fieldset>
-  HTML
  
 end
 
