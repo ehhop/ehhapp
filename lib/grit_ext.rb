b297f1991951114bf6e3af97d43d0376112b77f5
finkelm <finkel.mark@gmail.com>
2014-01-27 00:39:12 -0500

    Patched History for the first instance

--- lib/grit_ext.rb
+++ lib/grit_ext.rb
@@ -3,3 +3,15 @@ module Grit
     attr_writer :name
   end
 end
+
+class BlobAlike
+    attr_accessor :name
+    attr_accessor :data
+    attr_accessor :id
+    
+    def initialize(name, data)
+      @name = name
+      @data = data
+      @id = nil
+    end
+end
